ac76016ed8c94fbd021d355de4b84b8ee53be9bd
Lisa Sherer <lisa.sherer@pcc.edu>
2017-10-22 16:10:54 -0700

    add main method that prints first feature

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,11 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by lisa.sherer on 10/22/2017
+ */
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("First Feature!");
+    }
 }
