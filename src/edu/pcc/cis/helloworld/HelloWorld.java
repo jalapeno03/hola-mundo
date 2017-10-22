f6b285a498c4f8aef0add3b2d65fceb9ef7adb52
Lisa Sherer <lisa.sherer@pcc.edu>
2017-10-22 16:30:53 -0700

    print author

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("First Feature!");
         System.out.println(new Date());
+        System.out.println("By Lisa Sherer");
     }
 }
