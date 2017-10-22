4bbeef88c1e24546365a1a33e88e80deb0f9c50a
Lisa Sherer <lisa.sherer@pcc.edu>
2017-10-22 16:14:37 -0700

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by lisa.sherer on 10/22/2017
  */
@@ -7,5 +9,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("First Feature!");
+        System.out.println(new Date());
     }
 }
